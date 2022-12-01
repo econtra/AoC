5332c40bdc9bd4aaa5c5bfd5460915b7bf7c1e6e
Steffen Thyge Pedersen <stp@pfa.dk>
2022-12-01 10:57:03 +0100

    AoC2022d1

--- AdventOfCode/AoC2022/AoC2022/Library.fs
+++ AdventOfCode/AoC2022/AoC2022/Library.fs
@@ -14,23 +14,36 @@ type ``Results`` () =
 
     [<TestCase(1)>]
     member _.``1`` (dag : int) =
-        let input = System.IO.File.ReadAllLines (sprintf @"H:\AdventOfCode\Data\2022\%i.txt" dag)
-                    |> Array.map (fun s -> s.Substring(0,1),s.Substring(1) |> int)
-                    |> Array.fold (fun s t -> match fst t with | "+" -> s + (snd t) | _ -> s - (snd t)) 0
+        let input = System.IO.File.ReadAllLines (sprintf @"C:\Users\STP\source\repos\econtra\AoC\AdventOfCode\Data\2022\%i.txt" dag)
+                    |> Array.toList
+
+        let result1 = input
+                      |> List.fold (
+                                        fun state s -> if s = "" then
+                                                         0 :: state
+                                                       else
+                                                         (state.Head + int s) :: state.Tail
+                                   )
+                                   [0]
+                      |> List.sortDescending
+
+
+
+        result1.Head
+        |> printfn "%A"
+
+        result1
+        |> List.take 3
+        |> List.sum
+        |> printfn "%A"
+
+        Assert.Pass()
+
+    [<TestCase(2)>]
+    member _.``2`` (dag : int) =
+        let input = System.IO.File.ReadAllLines (sprintf @"C:\Users\STP\source\repos\econtra\AoC\AdventOfCode\Data\2022\%i.txt" dag)
 
         let result = input
 
         printfn "%A" result
         Assert.Pass()
-
-    [<TestCase(2)>]
-    member _.``2`` (dag : int) =
-        let input = System.IO.File.ReadAllLines (sprintf @"H:\AdventOfCode\Data\2022\%i.txt" dag)
-                    |> Seq.ofArray
-
-        let result = 2
-
-        printfn "%A" result
-        Assert.Pass()
-
-
