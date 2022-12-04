8f357b27516faab40cc98ba1a3244d1529ecd834
Steffen Thyge Pedersen <stp@pfa.dk>
2022-12-04 14:06:14 +0100

    3&4

--- AdventOfCode/AoC2022/AoC2022/Library.fs
+++ AdventOfCode/AoC2022/AoC2022/Library.fs
@@ -57,12 +57,12 @@ let parseToHands2 (entry : string array) =
             | "B", "Z"
                 -> Scissors
     let theirs = match entry.[0] with
-            | "A"
-                -> Rock
-            | "B"
-                -> Paper
-            | "C"
-                -> Scissors
+                 | "A"
+                     -> Rock
+                 | "B"
+                     -> Paper
+                 | "C"
+                     -> Scissors
     theirs,my
 
 [<TestFixture>]
@@ -119,5 +119,90 @@ type ``Results`` () =
         resulx
         |> Array.sum
         |> printfn "%A"
+
+        Assert.Pass()
+
+    [<Test>]
+    member _.``3`` () =
+        let dag = TestContext.CurrentContext.Test.MethodName
+        let input = System.IO.File.ReadAllLines (sprintf @"C:\Users\STP\source\repos\econtra\AoC\AdventOfCode\Data\2022\%s.txt" dag)
+
+        let charToInt c =
+            let conv n = int n - int 'a' + 1
+            if c |> Char.IsUpper then
+                c |> Char.ToLower |> conv |> (+) 26
+            else
+                conv c
+
+
+        let sackPairs = input
+                        |> Array.map (fun s -> let halfLenght   = s.Length/2
+                                               let first        = s.Substring(0,halfLenght) |> Set.ofSeq
+                                               let second       = s.Substring(halfLenght,halfLenght) |> Set.ofSeq
+
+                                               first,second
+                                     )
+
+        let result1 = sackPairs
+                      |> Array.map (fun t -> t ||> Set.intersect)
+                      |> Array.map (fun s -> s.MaximumElement |> charToInt)
+                      |> Array.sum
+                      |> printfn "%A"
+
+
+        let result2 = input
+                      |> Array.chunkBySize 3
+                      |> Array.map (fun group -> group
+                                                 |> Array.map Set.ofSeq
+                                                 |> Set.intersectMany
+                                   )
+                      |> Array.map (fun s -> s.MaximumElement |> charToInt)
+                      |> Array.sum
+                      |> printfn "%A"
+
+
+
+        Assert.Pass()
+
+    [<Test>]
+    member _.``4`` () =
+        let dag = TestContext.CurrentContext.Test.MethodName
+        let input = System.IO.File.ReadAllLines (sprintf @"C:\Users\STP\source\repos\econtra\AoC\AdventOfCode\Data\2022\%s.txt" dag)
+
+        let elfPairs = input
+                       |> Array.map (fun s -> let split = s.Split(',')
+                                                          |> Array.map(fun elf -> let borders = elf.Split('-')
+                                                                                                |> Array.map int
+                                                                                                |> Array.sort
+                                                                                  [borders |> Array.min .. borders |> Array.max]
+                                                                                  |> Set.ofSeq
+                                                                      )
+                                              split.[0], split.[1]
+                                    )
+
+        let result1 = elfPairs
+                      |> Array.map (fun pair -> let union = pair ||> Set.union
+                                                if union = fst pair || union = snd pair then true else false)
+                      |> Array.filter ((=) true)
+                      |> Array.length
+                      |> printfn "%i"
+
+        let result2 = elfPairs
+                      |> Array.map (fun pair -> if pair ||> Set.intersect |> Set.isEmpty then false else true)
+                      |> Array.filter ((=) true)
+                      |> Array.length
+                      |> printfn "%i"
+
+
+
+        Assert.Pass()
+
+    [<Test>]
+    member _.``5`` () =
+        let dag = TestContext.CurrentContext.Test.MethodName
+        let input = System.IO.File.ReadAllLines (sprintf @"C:\Users\STP\source\repos\econtra\AoC\AdventOfCode\Data\2022\%s.txt" dag)
+
+
+
 
         Assert.Pass()
