9f33ccf44c0c67d2b5f03ea4b2a231b7f84f0bd6
Steffen Thyge Pedersen <stp@pfa.dk>
2022-12-04 15:48:24 +0100

    cleanup

--- AdventOfCode/AoC2022/AoC2022/Library.fs
+++ AdventOfCode/AoC2022/AoC2022/Library.fs
@@ -28,50 +28,16 @@ type Hand =
             | Paper -> 2
             | Scissors -> 3
 
-let parseToHands (entry : string array) =
-    let parse (s : string) =
-        match s with
-            | "A"
-            | "X"
-                -> Rock
-            | "B"
-            | "Y"
-                -> Paper
-            | "C"
-            | "Z"
-                -> Scissors
-    parse entry.[0], parse entry.[1]
-
-let parseToHands2 (entry : string array) =
-    let my =match entry.[0], entry.[1] with
-            | "A", "Y"
-            | "B", "X"
-            | "C", "Z"
-                -> Rock
-            | "B", "Y"
-            | "C", "X"
-            | "A", "Z"
-                -> Paper
-            | "C", "Y"
-            | "A", "X"
-            | "B", "Z"
-                -> Scissors
-    let theirs = match entry.[0] with
-                 | "A"
-                     -> Rock
-                 | "B"
-                     -> Paper
-                 | "C"
-                     -> Scissors
-    theirs,my
-
 [<TestFixture>]
 type ``Results`` () =
 
-    [<TestCase(1)>]
-    member _.``1`` (dag : int) =
-        let input = System.IO.File.ReadAllLines (sprintf @"C:\Users\STP\source\repos\econtra\AoC\AdventOfCode\Data\2022\%i.txt" dag)
+    [<Test>]
+    member _.``1`` () =
+        let dag = TestContext.CurrentContext.Test.MethodName
+        let input = System.IO.File.ReadAllLines (sprintf @"C:\Users\STP\source\repos\econtra\AoC\AdventOfCode\Data\2022\%s.txt" dag)
                     |> Array.toList
+
+
 
         let result1 = input
                       |> List.fold (
@@ -95,10 +61,47 @@ type ``Results`` () =
 
         Assert.Pass()
 
-    [<TestCase(2)>]
-    member _.``2`` (dag : int) =
-        let input = System.IO.File.ReadAllLines (sprintf @"C:\Users\STP\source\repos\econtra\AoC\AdventOfCode\Data\2022\%i.txt" dag)
+    [<Test>]
+    member _.``2`` () =
+        let dag = TestContext.CurrentContext.Test.MethodName
+        let input = System.IO.File.ReadAllLines (sprintf @"C:\Users\STP\source\repos\econtra\AoC\AdventOfCode\Data\2022\%s.txt" dag)
 
+        let parseToHands (entry : string array) =
+            let parse (s : string) =
+                match s with
+                    | "A"
+                    | "X"
+                        -> Rock
+                    | "B"
+                    | "Y"
+                        -> Paper
+                    | "C"
+                    | "Z"
+                        -> Scissors
+            parse entry.[0], parse entry.[1]
+
+        let parseToHands2 (entry : string array) =
+            let my =match entry.[0], entry.[1] with
+                    | "A", "Y"
+                    | "B", "X"
+                    | "C", "Z"
+                        -> Rock
+                    | "B", "Y"
+                    | "C", "X"
+                    | "A", "Z"
+                        -> Paper
+                    | "C", "Y"
+                    | "A", "X"
+                    | "B", "Z"
+                        -> Scissors
+            let theirs = match entry.[0] with
+                         | "A"
+                             -> Rock
+                         | "B"
+                             -> Paper
+                         | "C"
+                             -> Scissors
+            theirs,my
 
         let result = input
                      |> Array.map (fun s -> s.Split(" "))
@@ -181,18 +184,18 @@ type ``Results`` () =
                                     )
 
         let result1 = elfPairs
-                      |> Array.map (fun pair -> let union = pair ||> Set.union
-                                                if union = fst pair || union = snd pair then true else false)
+                      |> Array.map (fun (p1,p2) -> let union = Set.union p1 p2
+                                                   if union = p1 || union = p2 then true else false)
                       |> Array.filter ((=) true)
                       |> Array.length
-                      |> printfn "%i"
 
         let result2 = elfPairs
                       |> Array.map (fun pair -> if pair ||> Set.intersect |> Set.isEmpty then false else true)
                       |> Array.filter ((=) true)
                       |> Array.length
-                      |> printfn "%i"
 
+        (result1,result2)
+        ||> printfn "%A,%A"
 
 
         Assert.Pass()
@@ -202,7 +205,9 @@ type ``Results`` () =
         let dag = TestContext.CurrentContext.Test.MethodName
         let input = System.IO.File.ReadAllLines (sprintf @"C:\Users\STP\source\repos\econtra\AoC\AdventOfCode\Data\2022\%s.txt" dag)
 
+        let result1 = 0
+        let result2 = 0
 
-
-
+        (result1,result2)
+        ||> printfn "%A,%A"
         Assert.Pass()
